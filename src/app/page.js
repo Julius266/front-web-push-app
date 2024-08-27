ec3106b496da4f65f062274c231ae92f0a144483
Julius266 <jvi3991zrj@gmail.com>
2024-08-27 09:37:19 -0500

    front

---
+++ src/app/page.js
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <div>
+      {/* Copia aquí el contenido de `index.html` transformado en JSX */}
+      <h1>Bienvenido a mi aplicación web push</h1>
+      {/* Ejemplo de cómo puedes transformar una etiqueta */}
+      {/* <div class="container"> se convierte en <div className="container"> */}
+    </div>
+  );
+}
